4a3e1e8154099e027bfa103884f5f48f88b46659
bohdan443 <bohdan443@gmail.com>
2021-05-08 20:04:05 +0300

    Create tabs in faq section

--- src/js/script.js
+++ src/js/script.js
@@ -2,102 +2,107 @@
 
 // @@include('_header.js')
 
+window.addEventListener('DOMContentLoaded', function () {
+    //  Проверка на моб. устройство
 
-//  Проверка на моб. устройство
-
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    any: function () {
-        return (
-            isMobile.Android() ||
-            isMobile.iOS() ||
-            isMobile.Opera()
-        );
-    }
-}
-
-if (isMobile.any()) {
-    document.body.classList.add('_touch');
-} else {
-    document.body.classList.add('_pc');
-}
-
-// Бургер меню
-
-const iconMenu = document.querySelector('.menu__icon');
-const menuBody = document.querySelector('.menu__body');
-if (iconMenu) {
-    iconMenu.addEventListener('click', function (e) {
-        document.body.classList.toggle('_lock');
-        iconMenu.classList.toggle('_active');
-        menuBody.classList.toggle('_active');
-    });
-}
-
-// Плавная прокрутка при клике
-
-const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
-if (menuLinks.length > 0) {
-    menuLinks.forEach(menuLink => {
-        menuLink.addEventListener("click", onMenuLinkClick);
-    });
-
-    function onMenuLinkClick(e) {
-        const menuLink = e.target;
-        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-            const gotoBlock = document.querySelector(menuLink.dataset.goto);
-            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
-
-            if (iconMenu.classList.contains('_active')) {
-                document.body.classList.remove('_lock')
-                iconMenu.classList.remove('_active');
-                menuBody.classList.remove('_active');
-            }
-            window.scrollTo({
-                top: gotoBlockValue,
-                behavior: "smooth"
-            });
-            e.preventDefault();
+    const isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        any: function () {
+            return (
+                isMobile.Android() ||
+                isMobile.iOS() ||
+                isMobile.Opera()
+            );
         }
     }
-}
 
-// ТАБЫ
+    if (isMobile.any()) {
+        document.body.classList.add('_touch');
+    } else {
+        document.body.classList.add('_pc');
+    }
 
-// const tabsBtn = document.querySelectorAll('.faq__tabs-btn');
-// const tabsItems = document.querySelectorAll('.faq__item');
+    // Бургер меню
 
-// tabsBtn.forEach(onTabClick);
+    const iconMenu = document.querySelector('.menu__icon');
+    const menuBody = document.querySelector('.menu__body');
+    if (iconMenu) {
+        iconMenu.addEventListener('click', function (e) {
+            document.body.classList.toggle('_lock');
+            iconMenu.classList.toggle('_active');
+            menuBody.classList.toggle('_active');
+        });
+    }
 
-// function onTabClick(item) {
-//     item.addEventListener('click', function () {
-//         let currentBtn = item;
-//         let tabId = currentBtn.getAttribute('data-tab');
-//         let currentTab = document.querySelector(tabId);
+    // Плавная прокрутка при клике
 
-//         if (!currentBtn.classList.contains('active-tab')) {
-//             tabsBtn.forEach(function (item) {
-//                 item.classList.remove('active-tab');
-//                 tabsBtn.classList.remove('active-tab');
-//             });
+    const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
+    if (menuLinks.length > 0) {
+        menuLinks.forEach(menuLink => {
+            menuLink.addEventListener("click", onMenuLinkClick);
+        });
 
-//             tabsItems.forEach(function (item) {
-//                 item.classList.remove('active-tab');
-//                 tabsItems.classList.remove('actibe-tab')
-//             });
+        function onMenuLinkClick(e) {
+            const menuLink = e.target;
+            if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+                const gotoBlock = document.querySelector(menuLink.dataset.goto);
+                const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.header').offsetHeight;
 
-//             currentBtn.classList.add('active-tab');
-//             currentTab.classList.add('active-tab');
-//         }
-//     });
-// }
+                if (iconMenu.classList.contains('_active')) {
+                    document.body.classList.remove('_lock')
+                    iconMenu.classList.remove('_active');
+                    menuBody.classList.remove('_active');
+                }
+                window.scrollTo({
+                    top: gotoBlockValue,
+                    behavior: "smooth"
+                });
+                e.preventDefault();
+            }
+        }
+    }
 
-// document.querySelector('.faq__tabs-btn').click();
+    // ТАБЫ
+
+    let faqBtn = document.querySelectorAll('.faq__tabs-btn'),
+        faqNav = document.querySelector('.faq__tabs'),
+        faqContent = document.querySelectorAll('.faq__content');
+
+    function hideTabContent(active) {
+        for (let i = active; i < faqContent.length; i++) {
+            faqContent[i].classList.remove('_show');
+            faqContent[i].classList.add('_hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    function showTabContent(disable) {
+        if (faqContent[disable].classList.contains('_hide')) {
+            faqContent[disable].classList.remove('_hide');
+            faqContent[disable].classList.add('_show');
+        };
+    };
+
+    faqNav.addEventListener('click', function (event) {
+        let target = event.target;
+
+        if (target && target.classList.contains('faq__tabs-btn')) {
+            for (let i = 0; i < faqBtn.length; i++) {
+                if (target == faqBtn[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+});
